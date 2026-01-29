d435cb6e534e4e3297d4e107267a0534b2899f44
andyeisenberg <andyjeisenberg@gmail.com>
2026-01-29 14:52:49 -0500

    Latest build deployed.

--- course/lunr-pretext-search-index.js
+++ course/lunr-pretext-search-index.js
@@ -10,9 +10,9 @@ var ptx_lunr_docs = [
   "body": " Syllabus        Course Information     Day and Time  Lectures: TR 3:30 pm - 4:50 pm Gladfelter L013    Recitation  Section 1: M 12:00 pm - 12:50 pm Wachman 15  Section 2: W 12:00 pm - 12:50 pm Wachman 9  Section 3: F 12:00 pm - 12:50 pm Wachman 9    Office Hours  See times and location on Canvas.    Course Goals  To help prepare students learn basic concepts, techniques, and applications of ordinary differential equations.    Topics Covered  First order ordinary differential equations, linear second order ordinary differential equations, the Laplace transform, and possibly more as time permits.    Prerequisite  Math 1042 with a grade of C or better, or equivalent transfer credit.    Course Mode  In-person    Credits  3       Textbook and Other Required Materials     Textbook  We will use Notes on Diffy Qs , by Jiri Lebl, which is freely available.    Phone  You will need a device (phone or tablet) to take pictures of your quizzes\/tests and upload your work.       Assessments   Homework  There will be homework assignments throughout the course. The homework will be graded for completion. Doing homework problems is the primary way that you will learn and internalize the material seen in class. Plan to spend a substantial amount of time throughout the semester doing homework problems.    Quizzes  There will be in-class quizzes every other week (on Jan 20, Feb 3, Mar 10, Mar 24, and Apr 21). Quizzes give you some practice in a test-like environment without the same grade pressure as a test. The quizzes should be a signal to you about whether you're keeping up with the pace of the course.    Exams  This course will have two midterm exams and a Final Exam. Exams are the main opportunity for you to demonstrate your understanding of course material.     Test 1  Tuesday, February 17 (in class)    Test 2  Tuesday, April 7 (in class)    Final Exam  Tuesday, May 5, 1:00 pm - 3:00 pm       Makeup Policy  There are no quiz or exam makeups. The lowest quiz will be dropped and replaced with the recitation score.     Grading  The grade components and their weights are:    Component  Weight    Homework  5%    Quizzes  15%    Exam 1  24%    Exam 2  24%    Final Exam  32%    The lower cutoff for each letter grade is given in the table below.     Letter Grade  Lower Cutoff    A  93%    A-  90%    B+  87%    B  83%    B-  80%    C+  77%    C  73%    C-  70%    D+  65%    D  55%    F  0%     Attendance Policy  Attendance is expected and will be recorded. If you need to miss a class for some reason, it is your responsibility to get notes from another student for any material you've missed, and to arrange to turn in\/make up any in-class or collected work.    Attendance and Your Health  To achieve course learning goals, students must attend and participate in classes, according to the course requirements. However, if you have tested positive for or are experiencing symptoms of a contagious illness, you should not come to campus or attend in-person classes or activities. It is the student’s responsibility to contact me to create a plan for participation and engagement in the course as soon as you are able to do so, and to make a plan to complete all assignments in a timely fashion.    Expectations for Class Conduct  It is important to foster a respectful and productive learning environment that includes all students in our diverse community of learners. Our differences, some of which are outlined in the University's nondiscrimination statement, will add richness to this learning experience. Therefore, all opinions and experiences, no matter how different or controversial they may be perceived, must be respected in the tolerant spirit of academic discourse.    Disability Statement  Any student who has a need for accommodations based on the impact of a documented disability or medical condition should contact Disability Resources and Services (DRS) in Howard Gittis Student Center South, Rm 420 ( drs@temple.edu ; 215-204-1280) to request accommodations and learn more about the resources available to you. If you have a DRS accommodation letter to share with me, or you would like to discuss your accommodations, please contact me as soon as practical. I will work with you and with DRS to coordinate reasonable accommodations for all students with documented disabilities. All discussions related to your accommodations will be confidential.    Academic Freedom  Freedom to teach and freedom to learn are inseparable facets of academic freedom. The University has adopted a policy on Student and Faculty Academic Rights and Responsibilities (Policy # 03.70.02) which can be accessed here .    Add\/Drop Policy  Students will be charged for a course unless dropped by the Drop\/Add deadline date. Check the University calendar for exact dates.  During the Drop\/Add period, students may drop a course with no record of the class appearing on their transcript. Students are not financially responsible for any courses dropped during this period. In the following weeks prior to or on the withdrawal date students may withdraw from a course with the grade of \"W\" appearing on their transcript. After the withdrawal date students may not withdraw from courses. Check the University calendar for exact dates. See the full policy by clicking here .    AI Policy  The use of generative AI tools (such as ChatGPT, DALL-E, etc.) is not permitted in this class unless specifically announced for a particular assignment; therefore, any use of AI tools for work in this class may be considered a violation of Temple University's Academic Honesty policy and Student Conduct Code, since the work is not your own. The use of unauthorized AI tools will result in a grade of zero on the assignment; a second offense will be reported to the Student Conduct Board.    Incomplete Grades  The grade \"I\" (an \"incomplete\") is only given if students cannot complete the course work due to circumstances beyond their control. It is necessary for the student to have completed the majority of the course work with a passing average and to sign an incomplete contract which clearly states what is left for the student to do and the deadline by which the work must be completed. The incomplete contract must also include a default grade that will be used in case the \"I\" grade is not resolved by the agreed deadline. See the full policy by clicking here .    Student Support Services  The following academic support services are available to students:      The Math Consulting Center      Student Success Center      University Libraries      Undergraduate Research Support      Career Center      Tuttleman Counseling Services      Disability Resources and Services      If you are experiencing food insecurity or financial struggles, Temple provides resources and support. Notably, the Temple University Cherry Pantry and the Temple University Emergency Student Aid Program are in operation as well as a variety of resources from the Division of Student Affairs.    "
 },
 {
-  "id": "course-notes-3",
+  "id": "notes-01-13",
   "level": "1",
-  "url": "course-notes-3.html",
+  "url": "notes-01-13.html",
   "type": "Section",
   "number": "",
   "title": "Tuesday, Jan 13",
@@ -21,7 +21,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-Introduction-2",
   "level": "2",
-  "url": "course-notes-3.html#subsec-Introduction-2",
+  "url": "notes-01-13.html#subsec-Introduction-2",
   "type": "Example",
   "number": "1",
   "title": "",
@@ -30,7 +30,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-Introduction-3",
   "level": "2",
-  "url": "course-notes-3.html#subsec-Introduction-3",
+  "url": "notes-01-13.html#subsec-Introduction-3",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
@@ -39,7 +39,7 @@ var ptx_lunr_docs = [
 {
   "id": "def-DE-solution",
   "level": "2",
-  "url": "course-notes-3.html#def-DE-solution",
+  "url": "notes-01-13.html#def-DE-solution",
   "type": "Definition",
   "number": "2",
   "title": "",
@@ -48,7 +48,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-Introduction-5",
   "level": "2",
-  "url": "course-notes-3.html#subsec-Introduction-5",
+  "url": "notes-01-13.html#subsec-Introduction-5",
   "type": "Example",
   "number": "3",
   "title": "",
@@ -57,7 +57,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-Introduction-6",
   "level": "2",
-  "url": "course-notes-3.html#subsec-Introduction-6",
+  "url": "notes-01-13.html#subsec-Introduction-6",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
@@ -66,7 +66,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-Introduction-7",
   "level": "2",
-  "url": "course-notes-3.html#subsec-Introduction-7",
+  "url": "notes-01-13.html#subsec-Introduction-7",
   "type": "Example",
   "number": "4",
   "title": "",
@@ -75,7 +75,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-Introduction-8",
   "level": "2",
-  "url": "course-notes-3.html#subsec-Introduction-8",
+  "url": "notes-01-13.html#subsec-Introduction-8",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
@@ -84,7 +84,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-Slope-Fields-2",
   "level": "2",
-  "url": "course-notes-3.html#subsec-Slope-Fields-2",
+  "url": "notes-01-13.html#subsec-Slope-Fields-2",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
@@ -93,7 +93,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-Slope-Fields-3",
   "level": "2",
-  "url": "course-notes-3.html#subsec-Slope-Fields-3",
+  "url": "notes-01-13.html#subsec-Slope-Fields-3",
   "type": "Example",
   "number": "5",
   "title": "",
@@ -102,7 +102,7 @@ var ptx_lunr_docs = [
 {
   "id": "thm-Picard",
   "level": "2",
-  "url": "course-notes-3.html#thm-Picard",
+  "url": "notes-01-13.html#thm-Picard",
   "type": "Theorem",
   "number": "7",
   "title": "Picard’s Theorem.",
@@ -111,16 +111,16 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-Slope-Fields-5",
   "level": "2",
-  "url": "course-notes-3.html#subsec-Slope-Fields-5",
+  "url": "notes-01-13.html#subsec-Slope-Fields-5",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
   "body": "partial derivative "
 },
 {
-  "id": "course-notes-4",
+  "id": "notes-01-15",
   "level": "1",
-  "url": "course-notes-4.html",
+  "url": "notes-01-15.html",
   "type": "Section",
   "number": "",
   "title": "Thursday, Jan 15",
@@ -129,7 +129,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-EU-3",
   "level": "2",
-  "url": "course-notes-4.html#subsec-EU-3",
+  "url": "notes-01-15.html#subsec-EU-3",
   "type": "Example",
   "number": "8",
   "title": "",
@@ -138,7 +138,7 @@ var ptx_lunr_docs = [
 {
   "id": "def-separable",
   "level": "2",
-  "url": "course-notes-4.html#def-separable",
+  "url": "notes-01-15.html#def-separable",
   "type": "Definition",
   "number": "10",
   "title": "",
@@ -147,7 +147,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-Separable-Equations-6",
   "level": "2",
-  "url": "course-notes-4.html#subsec-Separable-Equations-6",
+  "url": "notes-01-15.html#subsec-Separable-Equations-6",
   "type": "Example",
   "number": "11",
   "title": "",
@@ -156,7 +156,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-Separable-Equations-7",
   "level": "2",
-  "url": "course-notes-4.html#subsec-Separable-Equations-7",
+  "url": "notes-01-15.html#subsec-Separable-Equations-7",
   "type": "Example",
   "number": "12",
   "title": "",
@@ -165,7 +165,7 @@ var ptx_lunr_docs = [
 {
   "id": "def-First-Order-Linear",
   "level": "2",
-  "url": "course-notes-4.html#def-First-Order-Linear",
+  "url": "notes-01-15.html#def-First-Order-Linear",
   "type": "Definition",
   "number": "13",
   "title": "",
@@ -174,7 +174,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-First-Order-Linear-3",
   "level": "2",
-  "url": "course-notes-4.html#subsec-First-Order-Linear-3",
+  "url": "notes-01-15.html#subsec-First-Order-Linear-3",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
@@ -183,7 +183,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-First-Order-Linear-4",
   "level": "2",
-  "url": "course-notes-4.html#subsec-First-Order-Linear-4",
+  "url": "notes-01-15.html#subsec-First-Order-Linear-4",
   "type": "Example",
   "number": "14",
   "title": "",
@@ -192,16 +192,16 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-First-Order-Linear-5",
   "level": "2",
-  "url": "course-notes-4.html#subsec-First-Order-Linear-5",
+  "url": "notes-01-15.html#subsec-First-Order-Linear-5",
   "type": "Example",
   "number": "15",
   "title": "",
   "body": "  Solve . Applying the IC :    "
 },
 {
-  "id": "course-notes-5",
+  "id": "notes-01-20",
   "level": "1",
-  "url": "course-notes-5.html",
+  "url": "notes-01-20.html",
   "type": "Section",
   "number": "",
   "title": "Tuesday, Jan 20",
@@ -210,7 +210,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-Mixing-3",
   "level": "2",
-  "url": "course-notes-5.html#subsec-Mixing-3",
+  "url": "notes-01-20.html#subsec-Mixing-3",
   "type": "Example",
   "number": "16",
   "title": "",
@@ -219,7 +219,7 @@ var ptx_lunr_docs = [
 {
   "id": "def-autonomous",
   "level": "2",
-  "url": "course-notes-5.html#def-autonomous",
+  "url": "notes-01-20.html#def-autonomous",
   "type": "Definition",
   "number": "17",
   "title": "",
@@ -228,7 +228,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-Autonomous-Equations-3",
   "level": "2",
-  "url": "course-notes-5.html#subsec-Autonomous-Equations-3",
+  "url": "notes-01-20.html#subsec-Autonomous-Equations-3",
   "type": "Example",
   "number": "18",
   "title": "",
@@ -237,16 +237,16 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-Autonomous-Equations-4",
   "level": "2",
-  "url": "course-notes-5.html#subsec-Autonomous-Equations-4",
+  "url": "notes-01-20.html#subsec-Autonomous-Equations-4",
   "type": "Example",
   "number": "19",
   "title": "",
   "body": "  Consider . This equation is separable. It has equilibrium solutions when or . We can sketch a picture called a phase diagram of equilibrium solutions, along with typical non-equilibrium solutions:  TODO   "
 },
 {
-  "id": "course-notes-6",
+  "id": "notes-01-22",
   "level": "1",
-  "url": "course-notes-6.html",
+  "url": "notes-01-22.html",
   "type": "Section",
   "number": "",
   "title": "Thursday, Jan 22",
@@ -255,7 +255,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-exact-equations-3",
   "level": "2",
-  "url": "course-notes-6.html#subsec-exact-equations-3",
+  "url": "notes-01-22.html#subsec-exact-equations-3",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
@@ -264,7 +264,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-exact-equations-4",
   "level": "2",
-  "url": "course-notes-6.html#subsec-exact-equations-4",
+  "url": "notes-01-22.html#subsec-exact-equations-4",
   "type": "Fact",
   "number": "20",
   "title": "",
@@ -273,7 +273,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-exact-equations-5",
   "level": "2",
-  "url": "course-notes-6.html#subsec-exact-equations-5",
+  "url": "notes-01-22.html#subsec-exact-equations-5",
   "type": "Example",
   "number": "21",
   "title": "",
@@ -282,7 +282,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-exact-equations-7",
   "level": "2",
-  "url": "course-notes-6.html#subsec-exact-equations-7",
+  "url": "notes-01-22.html#subsec-exact-equations-7",
   "type": "Example",
   "number": "22",
   "title": "",
@@ -291,25 +291,25 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-exact-equations-9",
   "level": "2",
-  "url": "course-notes-6.html#subsec-exact-equations-9",
+  "url": "notes-01-22.html#subsec-exact-equations-9",
   "type": "Example",
   "number": "23",
   "title": "",
   "body": "  Solve the initial value problem: First, put the equation in a standard form: So the equation is exact. Then (an alternative to the ping-pong method): Putting these together, . So the implicit solution is: Applying the IC: We can find an explicit solution: The IC decides the sign:    "
 },
 {
-  "id": "course-notes-7",
+  "id": "notes-01-27",
   "level": "1",
-  "url": "course-notes-7.html",
+  "url": "notes-01-27.html",
   "type": "Section",
   "number": "",
   "title": "Tuesday, Jan 27",
   "body": " Tuesday, Jan 27   This is an outline of the topics we covered. You should not rely only on my notes. Make sure to take your own notes, and, if you miss a class, get notes from another student in class.    Integrating Factor  Given with (so the equation is not exact), can we find some to multiply by to get an exact equation? That is, we want such that: is exact, which requires . Hence: which is a partial differential equation, beyond the scope of this course.  What if we assume is a function of only one variable? Then: If is an expression in terms of only , then this is a first order differential equation in terms of .  Similarly, if , then: will be a first order differential equation in terms of if only involves .    Consider the equation: Then , so the equation is not exact. Then: which only involves . So we can set up and solve the differential equation: Multiplying the original equation, we get: So this equation is exact. So the implicit solution to the equation is:       Second Order Linear ODEs    A second order linear ODE can be put in the form: If , the equation is called homogeneous .     Superposition   If are solutions to a homogeneous equation, then so are: for any constants       Functions are linearly independent if the only way to have: is     For , are linearly independent if and only if they are not constant multiples of each other.    If are linearly independent solutions to a second order homogeneous equation, then the general solution is:     Note that this is a different conclusion from the superposition theorem, which says \"these functions are also solutions\" but doesn't guarantee that it's a complete list of all solutions.    Consider Let . Then: We can easily check: So are solutions to the equation. They are also linearly independent, since they're not multiples of each other. Therefore the general solution to the equation is:      "
 },
 {
-  "id": "course-notes-7-3-5",
+  "id": "notes-01-27-3-5",
   "level": "2",
-  "url": "course-notes-7.html#course-notes-7-3-5",
+  "url": "notes-01-27.html#notes-01-27-3-5",
   "type": "Example",
   "number": "24",
   "title": "",
@@ -318,7 +318,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-2nd-order-2",
   "level": "2",
-  "url": "course-notes-7.html#subsec-2nd-order-2",
+  "url": "notes-01-27.html#subsec-2nd-order-2",
   "type": "Definition",
   "number": "25",
   "title": "",
@@ -327,7 +327,7 @@ var ptx_lunr_docs = [
 {
   "id": "thm-superposition",
   "level": "2",
-  "url": "course-notes-7.html#thm-superposition",
+  "url": "notes-01-27.html#thm-superposition",
   "type": "Theorem",
   "number": "26",
   "title": "Superposition.",
@@ -336,7 +336,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-2nd-order-4",
   "level": "2",
-  "url": "course-notes-7.html#subsec-2nd-order-4",
+  "url": "notes-01-27.html#subsec-2nd-order-4",
   "type": "Definition",
   "number": "27",
   "title": "",
@@ -345,7 +345,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-2nd-order-6",
   "level": "2",
-  "url": "course-notes-7.html#subsec-2nd-order-6",
+  "url": "notes-01-27.html#subsec-2nd-order-6",
   "type": "Theorem",
   "number": "28",
   "title": "",
@@ -354,7 +354,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsec-2nd-order-8",
   "level": "2",
-  "url": "course-notes-7.html#subsec-2nd-order-8",
+  "url": "notes-01-27.html#subsec-2nd-order-8",
   "type": "Example",
   "number": "29",
   "title": "",
